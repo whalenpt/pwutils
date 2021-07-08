e0cbf2a861fbe006647c92f868680ee4470cef3f
Patrick Whalen <whalenpt@gmail.com>
2021-07-08 11:39:58 -0700

    Added reading one,two, and three column data file functionality to DataIO report class.

--- include/pwutils/report/dataio.hpp
+++ include/pwutils/report/dataio.hpp
@@ -21,6 +21,16 @@ class DataIO{
         template<typename T1,typename T2>
         void writeFile(const fs::path& fname,const std::vector<T1>& x,const std::vector<T2>& y) const;
 
+        // read one col data
+        template<typename T>
+        void readFile(const fs::path& fname,std::vector<T>&x) const;
+        // read two col data
+        template<typename T1,typename T2>
+        void readFile(const fs::path& fname,std::vector<T1>&x,std::vector<T2>&y) const;
+        // read three col data
+        template<typename T1,typename T2,typename T3>
+        void readFile(const fs::path& fname,std::vector<T1>&x,std::vector<T2>&y,std::vector<T3>&z) const;
+
         template<typename T>
         void appendFile(const fs::path& fname,const T& val) const;
         template<typename T1,typename T2>
@@ -78,6 +88,45 @@ void DataIO::appendFile(const fs::path& fname,const T1& val1,const T2& val2) con
          << std::setw(pw::REPORT_PRECISION + pw::REPORT_PADING) << val2 << std::endl; 
     fout.close();
 }
+
+template<typename T>
+void DataIO::readFile(const fs::path& fname,std::vector<T>&x) const
+{
+    fs::path file_path = m_dirpath / fname;
+    std::ifstream fin(file_path);
+    T val;
+    while(fin >> val)
+        x.push_back(val);
+    fin.close();
+}
+
+template<typename T1,typename T2>
+void DataIO::readFile(const fs::path& fname,std::vector<T1>&x,std::vector<T2>&y) const
+{
+    fs::path file_path = m_dirpath / fname;
+    std::ifstream fin(file_path);
+    T1 val1; T2 val2;
+    while(fin >> val1 >> val2){
+        x.push_back(val1);
+        y.push_back(val2);
+    }
+    fin.close();
+}
+
+template<typename T1,typename T2,typename T3>
+void DataIO::readFile(const fs::path& fname,std::vector<T1>&x,std::vector<T2>&y,std::vector<T3>&z) const
+{
+    fs::path file_path = m_dirpath / fname;
+    std::ifstream fin(file_path);
+    T1 val1; T2 val2; T3 val3;
+    while(fin >> val1 >> val2 >> val3){
+        x.push_back(val1);
+        y.push_back(val2);
+        z.push_back(val3);
+    }
+    fin.close();
+}
+
 
 
 
